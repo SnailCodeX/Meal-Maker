dfd91a3ac133b3721601c0ab8d024889d7fd790e
Plat <106413440+SnailCodeX@users.noreply.github.com>
2025-07-10 19:24:51 +0100

    index .js

---
+++ .js
@@ -0,0 +1,30 @@
+const menu = {
+  _meal: '',
+  _price: 0,
+  set meal(mealToCheck) {
+    if (typeof mealToCheck === 'string') {
+      this._meal = mealToCheck;
+    }
+  },
+  get meal() {
+    return this._meal;
+  },
+  set price(priceToCheck) {
+    if (typeof priceToCheck === 'number') {
+      this._price = priceToCheck;
+    }
+  },
+  get todaysSpecial() {
+    if (this._meal && this._price) {
+      return `Today's Special is ${this._meal} for ${this._price}!`;
+    } else {
+      return `Meal or price was not set correctly!`;
+    }
+  }
+};
+
+menu.meal = 'tortillas';
+menu.price = 100;
+console.log(menu.meal);
+console.log(menu.price);
+console.log(menu.todaysSpecial);
